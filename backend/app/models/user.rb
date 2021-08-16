0a3f64c1c53dab33dcff71221b70685d6bee0eb2
Paul Hsueh <hsueh.paul@gmail.com>
2021-08-16 18:00:36 -0400

    removed old diretory

--- backend/app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ActiveRecord::Base
-    has_many :jives, class_name: "Jive"
-    has_many :likes
- end
--- backend/config.ru
+++
@@ -1,4 +0,0 @@
-require './config/environment'
-
-use Rack::JSONBodyParser
-run ApplicationController
